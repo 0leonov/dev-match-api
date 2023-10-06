00a730bbc068e84cb03569aafc6987c85f7fa41f
Artyom Leonov <09leonov@gmail.com>
2023-10-06 22:14:14 +0300

    ConfigModule + TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { typeOrmModuleOptions } from './config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: typeOrmModuleOptions,
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
